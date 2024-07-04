a41653a70800933aed094413b64fcd35d2d97bd3
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-07-04 08:59:08 +0300

    comitted just now

--- js/app.js
+++ js/app.js
@@ -1,38 +1,76 @@
-const redBtn = document.querySelector("#red");
-const greenBtn = document.querySelector("#green");
-const yelloBtn = document.querySelector("#yellow");
-const blueBtn = document.querySelector("#blue");
+const redBtn = document.querySelector(".red");
+const greenBtn = document.querySelector(".green");
+const yelloBtn = document.querySelector(".yellow");
+const blueBtn = document.querySelector(".blue");
 const coloredBtn = document.querySelectorAll(".gameBtn");
 const startBtn = document.querySelector("#startBtn");
 const startArr = [];
 const playerArr = [];
+const background = document.querySelector("body");
+const score = document.querySelector("h1");
 let icounter = 0;
+let level = 0;
 
 const insertNum = () => {
-  let num = Math.floor(Math.random() * 4);
+  let num = Math.floor(Math.random() * 4 + 1);
   startArr.push(num);
+  return num;
 };
 
-const trigger = () => {
+const gameOver = () => {
+  startArr.length = 0;
+  playerArr.length = 0;
+  level = 0;
+  icounter = 0;
+};
+
+const trigger = (selected) => {
   setTimeout(() => {
-    coloredBtn[startArr[icounter]].classList.add("pressed");
+    document.getElementById(`${selected}`).classList.add("pressed");
   }, 0);
 
   setTimeout(() => {
-    coloredBtn[startArr[icounter]].classList.remove("pressed");
+    document.getElementById(`${selected}`).classList.remove("pressed");
   }, 500);
 };
 
-startBtn.addEventListener("click", () => {
-  insertNum();
-  trigger();
+const next = () => {
+  score.innerHTML = level;
+  const randNum = insertNum();
+  trigger(startArr[startArr.length - 1]);
+};
+
+startBtn.addEventListener("click", next);
+
+const checkMatch = (btn) => {
+  switch (true) {
+    case playerArr[icounter] == startArr[icounter]:
+      console.log("correct");
+      icounter++;
+
+      if (startArr.length == icounter) {
+        level++;
+        icounter = 0;
+        playerArr.length = 0;
+        setTimeout(() => {
+          next();
+        }, 1000);
+      }
+      break;
+    case !(playerArr[icounter] == startArr[icounter]):
+      console.log("lose");
+      //gameOver();
+      break;
+
+    default:
+      break;
+  }
+};
+
+coloredBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    playerArr.push(btn.id);
+    trigger(btn.id);
+    checkMatch(btn.id);
+  });
 });
-
-const checkMatch = (btn) => {};
-
-// coloredBtn.forEach((btn) => {
-//   btn.addEventListener("click",(btn.id)=>{
-
-//   });
-
-// });
